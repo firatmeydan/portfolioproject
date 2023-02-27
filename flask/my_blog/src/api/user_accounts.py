daaa0450217280d3a6dcb38c1018f29d8e149e54
firatmeydan <meydanfrt@gmail.com>
2023-02-26 21:43:40 -0500

    .

--- flask/my_blog/src/api/user_accounts.py
+++ flask/my_blog/src/api/user_accounts.py
@@ -14,41 +14,5 @@ def index():
 
 @bp.route('/<int:id>', methods=['GET'])
 def show(id: int):
-    t = User_Account.query.get_or_404(id)
-    return jsonify(t.serialize())
-
-
-# @bp.route('', methods=['POST'])
-# def create():
-#     # req body must contain user_id and content
-#     if 'user_id' not in request.json or 'content' not in request.json:
-#         return abort(400)
-#     # user with id of user_id must exist
-#     User.query.get_or_404(request.json['user_id'])
-#     # construct Tweet
-#     t = User(
-#         user_id=request.json['user_id'],
-#         content=request.json['content']
-#     )
-#     db.session.add(t) # prepare CREATE statement
-#     db.session.commit() # execute CREATE statement
-#     return jsonify(t.serialize())
-
-# @bp.route('/<int:id>', methods=['DELETE'])
-# def delete(id: int):
-#     t = User.query.get_or_404(id)
-#     try:
-#         db.session.delete(t) # prepare DELETE statement
-#         db.session.commit() # execute DELETE statement
-#         return jsonify(True)
-#     except:
-#         # something went wrong :(
-#         return jsonify(False)
-    
-# @bp.route('/<int:id>/liking_users', methods=['GET'])
-# def liking_users(id: int):
-#     t = Tweet.query.get_or_404(id)
-#     result = []
-#     for u in t.liking_users:
-#         result.append(u.serialize())
-#     return jsonify(result)
+    ua = User_Account.query.get_or_404(id)
+    return jsonify(ua.serialize())
